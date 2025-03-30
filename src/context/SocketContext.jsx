80a378d9cadbb4b11a5075319f69eee3c2fe74b8
zenric-atlasopen <zenric@atlasopen.com>
2025-03-30 23:01:36 +0800

    add vite prefix to env vars

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -8,7 +8,7 @@ const SocketProvider = ({ children }) => {
   const [socketId, setSocketId] = useState(null);
 
   useEffect(() => {
-    const newSocket = io(import.meta.env.SERVER_URL, { autoConnect: false });
+    const newSocket = io(import.meta.env.VITE_SERVER_URL, { autoConnect: false });
 
     newSocket.connect();
 
--- src/helpers/axiosHelpers.js
+++ src/helpers/axiosHelpers.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const callPublicRoute = async (requestConfig) => {
   const axiosResponse = await axios({
-    baseURL: `${import.meta.env.SERVER_URL}/api`,
+    baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
     ...requestConfig
   });
   return axiosResponse;
